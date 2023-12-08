fe7aba453ae080d16b79dc275a290d6ea280273f
nkenbou <nkenbou@gmail.com>
2023-12-08 22:22:51 +0000

    Next.js Page story example

--- apps/web/.storybook/main.ts
+++ apps/web/.storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
-import { join, dirname } from "path";
+import { dirname, join } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -10,10 +10,8 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  staticDirs: [{ from: "../public", to: "/" }],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
---
+++ apps/web/app/page.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Page from "./page";
+
+const meta = {
+  component: Page,
+} satisfies Meta<typeof Page>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default = {} satisfies Story;
