822ce4157496570d688864d368af8da3f3e908e5
nkenbou <nkenbou@gmail.com>
2024-02-24 22:51:35 +0000

    typed-css-modules

---
+++ apps/web/src/app/page.module.css.d.ts
@@ -0,0 +1,26 @@
+declare const styles: {
+  readonly "backgroundGradient": string;
+  readonly "card": string;
+  readonly "center": string;
+  readonly "circles": string;
+  readonly "code": string;
+  readonly "content": string;
+  readonly "description": string;
+  readonly "glowConic": string;
+  readonly "gradient": string;
+  readonly "gradientLarge": string;
+  readonly "gradientSmall": string;
+  readonly "grid": string;
+  readonly "hero": string;
+  readonly "heroContent": string;
+  readonly "logo": string;
+  readonly "logoGradient": string;
+  readonly "logoGradientContainer": string;
+  readonly "logos": string;
+  readonly "main": string;
+  readonly "turborepoWordmark": string;
+  readonly "turborepoWordmarkContainer": string;
+  readonly "vercelLogo": string;
+};
+export = styles;
+
